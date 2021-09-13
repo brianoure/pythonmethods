8da9e3dec6c4009d0f0aafcf8ea9ce16cbebe29b
brianoure <61065227+brianoure@users.noreply.github.com>
2021-09-13 19:12:58 +0300

    Update string pattern.py

--- string pattern.py
+++ string pattern.py
@@ -1,18 +1,18 @@
+"""
+pattern_stringHEAD_stringTAIL_list( "brianxxxcvbpythonvvvvvvvvvgghhbrianpppfgpython","brian","python") returns ["xxxcvb","pppfg"]
+pattern_stringHEAD_stringTAIL_list( "susanvenezuelastronggghhsusancanadastrong","susan","strong") returns ["venezuela","canada"]
+pattern_stringHEAD_stringTAIL_list( "boatxvmotorvvvmotorvgghmotor","boat","motor") returns ["xv"]
+"""
 def pattern_stringHEAD_stringTAIL_list(test_string,head_string,tail_string):
   ptr_start_list=[]
   ptr_end_list=[]
   frame_string_list=[]
-    for j in range(len(test_string)):
-      for k in range(len(head_string)):
-        if((test_string[j+k])==head_string[k]):
-          ptr_start_list =ptr_start_list + []
-      for m in range(len(tail_string)):
-        if((test_string[j+m])==tail_string[m]):
-          ptr_end_list =ptr_end_list + []
-      number_of_frames_string = len( ptr_start_list )
-      if(len(ptr_end_list)<len(ptr_start_list)):
-        number_of_frames_string = len(len(ptr_end_list)
-      for x in range(number_of_frames_string):
-        frame_string_list= frame_string_list + test_string[ ptr_start_list[ x+len(head_string)]:ptr_end_list[x] ]
-      return frame_string_list
+  for j in range(len( test_string )):
+        if((test_string[j:j+len(head_string)]) == head_string):   ptr_start_list = ptr_start_list + [j]
+        if((test_string[j:j+len(tail_string)]) == tail_string):   ptr_end_list = ptr_end_list + [j]
+  number_of_frames = len( ptr_start_list )
+        if(len(ptr_end_list)<len(ptr_start_list)):  number_of_frames = len( ptr_end_list )
+  for x in range(number_of_frames_string):
+        frame_string_list = frame_string_list + test_string[ (ptr_start_list[ x+len(head_string)]) : ptr_end_list[x]   ]
+  return frame_string_list
                                       
